d71430d5195802f5366b2a323ae1a06a63c9c561
sanket7472 <sankettamnar2003@gmail.com>
2024-06-22 14:05:37 +0530

    Configurable UI

---
+++ js/index.js
@@ -0,0 +1,8 @@
+alert('Welcome to CINECLICK SUYOG TATHOD`s Web..!');
+function contactaler(){
+    alert('Thank you for contacting us. We will get back to you soon..!');
+  
+}
+document.getElementById(submit);{
+    onclick= contactalert()
+};
